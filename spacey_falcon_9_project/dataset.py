9b478ce3d95a56d64896d3be27e565c1f7917591
svd-jelo <skulpayls21@gmail.com>
2026-07-26 15:32:20 +0800

    Modify downloader functions to return download path instead of None

--- spacey_falcon_9_project/dataset.py
+++ spacey_falcon_9_project/dataset.py
@@ -6,6 +6,7 @@ import time
 from bs4 import BeautifulSoup
 import pandas as pd
 import requests
+import json
 
 #=====================================================================================================================
 # DATA COLLECTION PART 1 - FROM API
@@ -25,7 +26,7 @@ def get_ll2_launches(offset):
                 time.sleep(2 * (attempt + 1))
                 continue
             response.raise_for_status()
-            return response.content
+            return response.json()
 
         except requests.exceptions.HTTPError as e:
             print("Error: {}".format(e))
@@ -34,7 +35,8 @@ def get_ll2_launches(offset):
     print("Failed after 3 attempts (504 Server Error)")
     return None
 
-def download_all_ll2_launches():
+def download_all_ll2_launches() -> None|list[Path]:
+    file_paths = []
     for offset in range(0, 700, 100):
         parent_dir = Path.cwd().parent
         file_dir = parent_dir / "data" / "raw"
@@ -48,13 +50,15 @@ def download_all_ll2_launches():
             data = get_ll2_launches(offset)
             if not isinstance(data, bytes):
                 raise TypeError("Expected bytes, got {}".format(type(data)))
-            with open(file_path, "wb") as f:
-                f.write(data)
+            with open(file_path, "w") as f:
+                json.dump(data, f)
         except Exception as e:
             print("Error: {}".format(e))
             break
+        file_paths.append(file_path)
+    return file_paths
 
-def download_launch_data_static(url, file_name, query_params=None, headers=None):
+def download_launch_data_static(url: str, file_name: str, query_params=None, headers=None) -> None|Path:
     parent_dir = Path.cwd().parent
     file_dir = parent_dir / "data" / "raw"
     file_dir.mkdir(parents=True, exist_ok=True)
@@ -62,7 +66,7 @@ def download_launch_data_static(url, file_name, query_params=None, headers=None)
 
     if file_path.is_file():
         print("{} already exists".format(file_name))
-        return None
+        return file_path
 
     response = requests.get(url, params=query_params, headers=headers)
 
@@ -76,36 +80,38 @@ def download_launch_data_static(url, file_name, query_params=None, headers=None)
     try:
         with open(file_path, "wb") as f:
             f.write(response.content)
+        return file_path
     except Exception as e:
         print("Error: {}".format(e))
         return None
 
+
 # Download LL2 API Launches Data
-download_all_ll2_launches()
+#download_all_ll2_launches()
 
 # Download static JSON provided by the course for project use
-url_ibm='https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/datasets/API_call_spacex_api.json'
-file_name_ibm = 'ibm-ds-capstone-launch-data.json'
-download_launch_data_static(url_ibm, file_name_ibm)
+#url_ibm='https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/datasets/API_call_spacex_api.json'
+#file_name_ibm = 'ibm-ds-capstone-launch-data.json'
+#download_launch_data_static(url_ibm, file_name_ibm)
 
 # Download launch data from GCAT
-url_gcat='https://planet4589.org/space/gcat/tsv/launch/Falcon9.tsv'
-file_name_gcat = 'mcdowell-gcat-launch-data.tsv'
-download_launch_data_static(url_gcat, file_name_gcat)
+#url_gcat='https://planet4589.org/space/gcat/tsv/launch/Falcon9.tsv'
+#file_name_gcat = 'mcdowell-gcat-launch-data.tsv'
+#download_launch_data_static(url_gcat, file_name_gcat)
 
 #=====================================================================================================================
 # DATA COLLECTION PART 2 - BY WEB SCRAPING
 #=====================================================================================================================
 
 # Download launch data from Wikipedia
-url_wiki = "https://en.wikipedia.org/w/index.php?title=List_of_Falcon_9_and_Falcon_Heavy_launches&oldid=1027686922"
-file_name_wiki = "wikipedia-launch-data-table.html"
-headers_wiki = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                  "AppleWebKit/537.36 (KHTML, like Gecko) "
-                  "Chrome/91.0.4472.124 Safari/537.36"
-}
-download_launch_data_static(url_wiki, file_name_wiki, headers=headers_wiki)
+#url_wiki = "https://en.wikipedia.org/w/index.php?title=List_of_Falcon_9_and_Falcon_Heavy_launches&oldid=1027686922"
+#file_name_wiki = "wikipedia-launch-data-table.html"
+#headers_wiki = {
+#    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+#                  "AppleWebKit/537.36 (KHTML, like Gecko) "
+#                  "Chrome/91.0.4472.124 Safari/537.36"
+#}
+#download_launch_data_static(url_wiki, file_name_wiki, headers=headers_wiki)
 
 # Parse HTML document and convert to csv
 def parse_table(table) -> pd.DataFrame:
@@ -192,7 +198,7 @@ def parse_all_tables(past_launches_tables) -> pd.DataFrame:
 
     return launches_df_web_scrap.reset_index(drop=True)
 
-def launches_html_to_csv(csv_path:None|str = None, html_path='/Users/jelo/spacey_falcon_9_project/data/raw/wikipedia-launch-data-table.html') -> None:
+def launches_html_to_csv(html_path: str|Path, csv_path:None|str = None) -> None|Path:
     """
     :param csv_path: file path where converted HTML document is to be saved
     :param html_path: file path to the HTML document.
@@ -216,7 +222,10 @@ def launches_html_to_csv(csv_path:None|str = None, html_path='/Users/jelo/spacey
 
         if csv_path.is_file():
             print("{} already exists".format(file_name))
-            return None
+            return csv_path
+
+    if isinstance(csv_path, str):
+        csv_path = Path(csv_path)
 
     # Parse HTML document and store data in a pandas DataFrame
     html_launch_section = html_doc.find("section", attrs={"aria-labelledby": "Past_launches"})
@@ -226,4 +235,4 @@ def launches_html_to_csv(csv_path:None|str = None, html_path='/Users/jelo/spacey
     # Save pandas DataFrame to CSV file
     launches_df.to_csv(csv_path, index=False)
 
-    return None
+    return csv_path
